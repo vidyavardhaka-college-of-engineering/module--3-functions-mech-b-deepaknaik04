cc2d6de2ba313254f22dad7b57b2dcdc047bec40
deepaknaik04 <20me0123@vvce.ac.in>
2021-03-27 12:55:58 +0000

    p9

--- Program-9.c
+++ Program-9.c
@@ -3,7 +3,7 @@
 	output:The entered number is odd
 	*/
 	#include<stdio.h>
-	 //if the least significant bit is 1 the number is odd and 0 the number is even 
+	 int checkOdd(int );//if the least significant bit is 1 the number is odd and 0 the number is even 
 	int checkOdd(int n1)
 	{   
 	    return (n1 & 1);//The & operator does a bitwise and,
@@ -16,9 +16,7 @@
 	    // If checkOddEven() function returns 1 then the number is odd 
 	    if(checkOdd(n1))
 	    {
-        printf("the entered number is odd");
-        
-	        //enter the printf statement
+	      printf("The entered number is odd.");  //enter the printf statement
 	    }
 	    return 0;
 	}
